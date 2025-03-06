3dec7588ab8fe72bc75627e026020196ef7d64ea
drizzle72 <271578787@qq.com>
2025-03-07 03:28:26 +0800

    完善食物热量数据库：增加200多种食物、添加营养素信息和新功能

--- create_env.py
+++ create_env.py
@@ -4,7 +4,7 @@
 # 创建.env文件脚本
 
 with open('.env', 'w') as f:
-    f.write('DEEPSEEK_API_KEY=sk-cd33cdfab0de4bb3b220ecda7c123c69\n')
+    f.write('DEEPSEEK_API_KEY=xxa7c123c69\n')
     f.write('QWEN_API_KEY=78HabPBmvHBKIdbXdpzh8liGAKCU10\n')
 
 print("已成功创建.env文件！") 
